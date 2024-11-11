85e0f3cdb6572d2f126657c1b0f002b225b4d248
iampriyangkush <dpriyangkush004@gmail.com>
2024-11-11 23:29:21 +0530

    Company Dashboard Bottom Tab completed.

--- src/screens/jobposting/DashboardForCompany.js
+++ src/screens/jobposting/DashboardForCompany.js
@@ -1,12 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { BG_COLOR, TEXT_COLOR } from '@/src/utils/Colors';
+import { moderateScale, verticalScale, scale } from 'react-native-size-matters';
 
 const DashboardForCompany = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
-    <View>
-      <Text>DashboardForCompany</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.bottomView}>
+        <TouchableOpacity style={styles.bottomTab} onPress={() => {
+          setSelectedTab(0)
+        }}>
+          <Image source={require('../../images/home.png')} style={[styles.tabIcon, {tintColor: selectedTab == 0 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)'},]} />
+          {selectedTab === 0 && <View style={styles.selectedIndicator} />}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomTab} onPress={() => {
+          setSelectedTab(1)
+        }}>
+        <Image source={require('../../images/search-user.png')} style={[styles.tabIcon, {tintColor: selectedTab == 1 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)'},]}/>
+        {selectedTab === 1 && <View style={styles.selectedIndicator} />}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomTab} onPress={() => {
+          setSelectedTab(2)
+        }}>
+        <Image source={require('../../images/addition.png')} style={[styles.tabIcon, {tintColor: selectedTab == 2 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)'},]}/>
+        {selectedTab === 2 && <View style={styles.selectedIndicator} />}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomTab} onPress={() => {
+          setSelectedTab(3)
+        }}>
+        <Image source={require('../../images/chat.png')} style={[styles.tabIcon, {tintColor: selectedTab == 3 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)'},]} />
+        {selectedTab === 3 && <View style={styles.selectedIndicator} />}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomTab} onPress={() => {
+          setSelectedTab(4)
+        }}>
+        <Image source={require('../../images/user.png')} style={[styles.tabIcon, {tintColor: selectedTab == 4 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)'},]} />
+        {selectedTab === 4 && <View style={styles.selectedIndicator} />}
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
-export default DashboardForCompany
+export default DashboardForCompany;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: BG_COLOR,
+  },
+  bottomView: {
+    width: '100%',
+    height: verticalScale(60),
+    backgroundColor: '#FFF7D1',
+    shadowColor: BG_COLOR,
+    shadowOpacity: 1,
+    shadowOffset: {x: 0, y: 1},
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    
+  },
+  bottomTab: {
+    width: '20%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    
+  },
+  tabIcon: {
+    width: scale(24),
+    height: scale(24),
+    
+  },
+  selectedIndicator: {
+    width: '40%',
+    height: 4,
+    backgroundColor: '#8B5DFF',
+    marginTop: 6,
+    borderRadius: 2,
+    
+  }
+
+})
