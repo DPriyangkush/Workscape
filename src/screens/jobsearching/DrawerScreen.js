42b86df9b2ad7b30fe259c1762c61e57af73f913
91869@Priyangkush <dpriyangkush004@gmail.com>
2025-01-17 01:20:52 +0530

    Completed the bottom view of candidate section

--- src/screens/jobsearching/DrawerScreen.js
+++ src/screens/jobsearching/DrawerScreen.js
@@ -1,25 +1,44 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React,  { useState }from 'react'
 import { BG_COLOR } from '@/src/utils/Colors'
 import { scale, verticalScale } from 'react-native-size-matters'
+import { useNavigation } from 'expo-router'
+import Home from '../jobsearching/tabs/Home'
+import Search from '../jobsearching/tabs/Search'
+import Applies from '../jobsearching/tabs/Applies'
+import Profile from '../jobsearching/tabs/Profile'
 
 
 const DrawerScreen = () => {
-  
+  const [selectedTab, setSelectedTab] = useState(0);
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
+      {selectedTab == 0 ? <Home />: selectedTab == 1 ? <Search /> : selectedTab == 2 ? <Applies /> :  <Profile  onJobsClick={() => {setSelectedTab(0)}}/>}
       <View style={styles.bottomNavView}>
-        <TouchableOpacity style={styles.tabs}>
-          <Image source={require('../../images/home-3-line.png')} style={styles.tabIcon} />
+        <TouchableOpacity style={styles.tabs} onPress={() => {
+          setSelectedTab(0)
+        }}>
+          <Image source={require('../../images/home.png')} style={[styles.tabIcon, { tintColor: selectedTab == 0 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)' },]} />
+          {selectedTab === 0 && <View style={styles.selectedIndicator} />}
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tabs}>
-          <Image source={require('../../images/search-2-line.png')} style={styles.tabIcon} />
+        <TouchableOpacity style={styles.tabs} onPress={() => {
+          setSelectedTab(1)
+        }}>
+          <Image source={require('../../images/search.png')} style={[styles.tabIcon, { tintColor: selectedTab == 1 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)' },]} />
+          {selectedTab === 1 && <View style={styles.selectedIndicator} />}
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tabs}>
-          <Image source={require('../../images/jobs.png')} style={styles.tabIcon} />
+        <TouchableOpacity style={styles.tabs} onPress={() => {
+          setSelectedTab(2)
+        }}>
+          <Image source={require('../../images/applied.png')} style={[styles.tabIcon, { tintColor: selectedTab == 2 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)' },]} />
+          {selectedTab === 2 && <View style={styles.selectedIndicator} />}
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tabs}>
-          <Image source={require('../../images/user-3-line.png')} style={styles.tabIcon} />
+        <TouchableOpacity style={styles.tabs} onPress={() => {
+          setSelectedTab(3)
+        }}>
+          <Image source={require('../../images/user.png')} style={[styles.tabIcon, { tintColor: selectedTab == 3 ? '#8B5DFF' : 'rgba(15, 15, 15, 0.8)' },]} />
+          {selectedTab === 3 && <View style={styles.selectedIndicator} />}
         </TouchableOpacity>
       </View>
     </View>
@@ -51,12 +70,19 @@ const styles = StyleSheet.create({
   tabs: {
     width: '25%',
     height: '100%',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     
   },
   tabIcon: {
     width: scale(24),
     height: scale(24),
+  },
+  selectedIndicator: {
+    width: '30%',
+    height: 4,
+    backgroundColor: '#8B5DFF',
+    borderRadius: 2,
+
   }
 })
---
+++ src/screens/jobsearching/tabs/Applies.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Applies = () => {
+  return (
+    <View>
+      <Text>Applies</Text>
+    </View>
+  )
+}
+
+export default Applies
