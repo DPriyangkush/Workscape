57daec5dcdc3cbaea8c09864bc0bd413540cb81f
91869@Priyangkush <dpriyangkush004@gmail.com>
2025-01-19 03:13:34 +0530

    Custom Drawer Completed.

--- src/screens/jobsearching/CustomDrawer.js
+++ src/screens/jobsearching/CustomDrawer.js
@@ -1,12 +1,134 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { BG_COLOR, TEXT_COLOR } from '@/src/utils/Colors'
+import { moderateScale, moderateVerticalScale, scale, verticalScale } from 'react-native-size-matters'
+import { FlatList } from 'react-native-gesture-handler'
+import index from '@/app/(tabs)'
 
 const CustomDrawer = () => {
   return (
-    <View>
-      <Text>CustomDrawer</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topView}>
+      <Image source={require('../../images/profile.png')} style={styles.profile} />
+      <View>
+        <Text style={styles.heading}>Build Your Profile</Text>
+        <Text style={styles.subHeading}>Connecting you with opportunities at WorkScape!</Text>
+      </View>
+      </View>
+      
+      <View style={styles.btnViews}>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={[styles.btnText, {color: BG_COLOR}]}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.SignupBtn}>
+          <Text style={styles.btnText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.seperator}></View>
+
+      <FlatList 
+      data={[{title: 'Rate us', icon: require('../../images/contact_us.png')}, {title: 'Theme', icon: require('../../images/theme.png')}, {title: 'Know More', icon: require('../../images/knowmore.png')}]}
+      renderItem={({item, index}) => {
+        return(
+          <TouchableOpacity style={styles.menuItem}>
+            <View style={styles.menuItemLeftView}>
+              <Image source={item.icon} style={styles.menuItemIcon} />
+              <Text style={styles.title}>{item.title}</Text>
+            </View>
+            <Image source={require('../../images/right.png')} />
+          </TouchableOpacity>
+        )
+      }} />
+    </SafeAreaView>
   )
 }
 
 export default CustomDrawer
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: BG_COLOR
+  },
+  profile: {
+    width: scale(50),
+    height: scale(50),
+    marginLeft: moderateScale(10),
+    marginTop: verticalScale(20),
+  },
+  topView: {
+    flexDirection: 'row',
+    marginTop: moderateScale(10)
+  },
+  heading: {
+    fontSize: 18,
+    width: '70%',
+    fontWeight: '700',
+    marginLeft: moderateScale(10),
+    marginTop: moderateScale(20),
+
+  },
+  subHeading: {
+    fontSize: moderateScale(12),
+    width: '60%',
+    marginLeft: moderateScale(10),
+    marginTop: moderateScale(2),
+  },
+  btnViews: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignContent: 'center',
+    marginTop: moderateVerticalScale(20),
+  },
+  loginBtn: {
+    width: '40%',
+    height: verticalScale(30),
+    backgroundColor: TEXT_COLOR,
+    borderWidth: 1,
+    borderRadius: moderateScale(30),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  SignupBtn: {
+    width: '40%',
+    height: verticalScale(30),
+    borderColor: TEXT_COLOR,
+    borderWidth: 1,
+    borderRadius: moderateScale(30),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnText: {
+    fontWeight: '500',
+    fontSize: moderateScale(15),
+  },
+  seperator: {
+    width: "90%",
+    height: moderateScale(.5),
+    backgroundColor: TEXT_COLOR,
+    alignSelf: "center",
+    marginTop: moderateScale(20),
+  },
+  menuItem: {
+    width: "90%",
+    alignSelf: 'center',
+    height: verticalScale(50),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  menuItemLeftView: {
+    flexDirection: 'row',
+    alignItems: "center",
+  },
+  menuItemIcon: {
+    width: scale(24),
+    height: scale(24),
+  },
+  title: {
+    fontSize: moderateScale(15),
+    fontWeight: 600,
+    marginLeft: moderateScale(10),
+  }
+})
--- src/screens/jobsearching/Main.js
+++ src/screens/jobsearching/Main.js
@@ -3,12 +3,13 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import DrawerScreen from '../jobsearching/DrawerScreen';
 import CustomDrawer from '../jobsearching/CustomDrawer';
+import { TEXT_COLOR } from '@/src/utils/Colors';
 
 const Drawer = createDrawerNavigator();
 
 const Main = () => {
   return (
-    <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props}/>}>
+    <Drawer.Navigator screenOptions={{headerTintColor: TEXT_COLOR}} drawerContent={(props) => <CustomDrawer {...props}/>}>
         <Drawer.Screen name='Drawer' component={DrawerScreen} options={{title: "WorkScape"}}/>
     </Drawer.Navigator>
   )
